b52409fb22b11dc4f2ed1cce03eef5180aa66055
Воронин Михаил <mavoronin14@ya.ru>
2022-11-15 22:49:59 +0500

    Initial commit

---
+++ src/test/java/mvoronin/lr5/Lr5ApplicationTests.java
@@ -0,0 +1,13 @@
+package mvoronin.lr5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lr5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
